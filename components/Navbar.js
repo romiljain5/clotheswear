3dcf4604d9c0e147f2ecc109aec6de1b2dc64308
Romil Jain <61315825+jainromil56@users.noreply.github.com>
2022-03-28 11:31:45 +0530

    Made cart sidebar
    Used reactIcons and tailwindcss, Also use useRef for targetting box using onclick function toggleCart.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Link from 'next/link'
 import {BsCart2} from 'react-icons/bs'
+import {AiFillCloseSquare, AiFillPlusCircle, AiFillMinusCircle} from 'react-icons/ai'
+import {BsBagCheck} from 'react-icons/bs'
 
 const Navbar = () => {
+  // This function is used to toggle cart
+  const toggleCart =()=>{
+    if(ref.current.classList.contains('translate-x-full')){
+      ref.current.classList.remove('translate-x-full')
+      ref.current.classList.add('translate-x-0')
+    }else if(!ref.current.classList.contains('transform-x-full')){
+      ref.current.classList.remove('translate-x-0')
+      ref.current.classList.add('translate-x-full')
+    }
+  }
+
+  const ref = useRef()
   return (
     <div className='shadow-md m-0 p-0'>
       <div className="container w-11/12 justify-start flex flex-col md:flex-row w-full mx-auto py-1">
@@ -24,9 +38,30 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div className='flex md:ml-auto my-auto justify-center flex-wrap'>
-          <Link href={'/tshirts'}><a><BsCart2 className='text-3xl'/></a></Link>
+      <div onClick={toggleCart} className='cart cursor-pointer flex md:ml-auto my-auto justify-center flex-wrap'>
+          <a><BsCart2 className='text-3xl'/></a>
       </div>
+
+      </div>
+
+      <div ref={ref} className="sidebar h-full z-10 shadow-xl  fixed top-0 right-0 bg-orange-200 py-10 md:w-2/6 px-8 w-3/5 transform transition-transform translate-x-full">
+        <h2 className="text-xl font-bold">Shopping Cart</h2>
+        <span onClick={toggleCart} className="absolute top-5 right-2 text-2xl cursor-pointer"><AiFillCloseSquare/></span>
+        <ol className="list-decimal">
+          <li className="my-2">
+            <div className="flex">
+              <div className="w-2/3">Tshirt - Green Topper</div>
+              <div className="w-1/6 flex items-center justify-center text-2xl cursor-pointer"><AiFillMinusCircle/></div>
+              <div className="w-1/6 flex items-center justify-center bg-white">1</div>
+              <div className="w-1/6 flex items-center justify-center text-2xl cursor-pointer"><AiFillPlusCircle/></div>
+            </div>
+          </li>
+        </ol>
+
+        <button class="flex mx-auto mt-16 text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">
+          <BsBagCheck className="my-1 mr-5"/> Checkout
+        </button>
+
       </div>
     </div>
   )
